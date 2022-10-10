40a417985f7889551a8c8d8631a5821b0bb458d6
RaggedyRagz <JohnArigo96706@gmail.com>
2022-10-10 13:33:42 -0700

    pre-deployed

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
